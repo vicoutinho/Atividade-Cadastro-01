57974a1b2f96b051cf8e64ba5cbe14ac33160f0a
Vicoutinho <vitoriacoutinhosilva12@gmail.com>
2024-04-02 20:45:32 -0300

    Atividade feita para treinar o comando Jpanel no Java

---
+++ src/main/java/com/mycompany/cadastrocliente/CadastroCliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrocliente;
+
+/**
+ *
+ * @author Vitoria
+ */
+public class CadastroCliente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
